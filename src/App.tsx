16447e0ff0745d6da8f2a7e9211f78bd2452f629
blackcater <blackcater2015@gmail.com>
2021-05-21 14:08:33 +0800

    feat: add SegmentedControl element

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
-import { Button } from "@mantine/core";
+import { Button, SegmentedControl } from "@mantine/core";
+
+const data = [
+  { label: 'React', value: 'react' },
+  { label: 'Angular', value: 'ng' },
+  { label: 'Vue', value: 'vue' },
+  { label: 'Svelte', value: 'svelte' },
+];
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [value, onChange] = useState('react');
 
   return (
     <div className="App">
@@ -18,6 +27,9 @@ function App() {
         </p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <SegmentedControl data={data} value={value} onChange={onChange} />
         </p>
         <p>
           <a
